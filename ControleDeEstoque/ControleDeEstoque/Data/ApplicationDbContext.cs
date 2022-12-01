572459fd26d885e4d4492bdf5823af306f5a8b38
Giovane <giovane@hotmail.com.br>
2022-12-01 13:20:19 -0300

    Versão 1.0

---
+++ ControleDeEstoque/ControleDeEstoque/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ControleDeEstoque.Models;
+
+namespace ControleDeEstoque.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ControleDeEstoque.Models.Produto> Produto { get; set; }
+    }
+}
---
+++ ControleDeEstoque/ControleDeEstoque/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeEstoque.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição do produto")]
+        public string? Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe a quantidade do produto")]
+        [Min(0, ErrorMessage = "O valor deve ser maior ou igual a 0")]
+        public int Quantidade { get; set; }
+    }
+}
